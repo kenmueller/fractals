2e77bd6c905bf30ae79bc65275dd27814222aeea
Ken Mueller <kenmueller0@gmail.com>
2019-04-27 18:48:51 -0700

    shortened drawFractal

--- index.js
+++ index.js
@@ -17,14 +17,11 @@ const drawTriangle = point1 => point2 => point3 => color => {
 }
 
 const drawFractal = point1 => point2 => point3 => color => depth => {
-	if (depth > 0) {
-		const pointA = { x: point1.x + (point2.x - point1.x) / 2, y: point1.y - (point1.y - point2.y) / 2 }
-		const pointB = { x: point2.x + (point3.x - point2.x) / 2, y: point2.y - (point2.y - point3.y) / 2 }
-		const pointC = { x: point1.x + (point3.x - point1.x) / 2, y: point1.y }
-		drawFractal(point1)(pointA)(pointC)(color)(depth - 1)
-		drawFractal(pointA)(point2)(pointB)(color)(depth - 1)
-		drawFractal(pointC)(pointB)(point3)(color)(depth - 1)
-	} else {
-		drawTriangle(point1)(point2)(point3)(color)
-	}
+	if (!depth) return drawTriangle(point1)(point2)(point3)(color)
+	const pointA = { x: point1.x + (point2.x - point1.x) / 2, y: point1.y - (point1.y - point2.y) / 2 }
+	const pointB = { x: point2.x + (point3.x - point2.x) / 2, y: point2.y - (point2.y - point3.y) / 2 }
+	const pointC = { x: point1.x + (point3.x - point1.x) / 2, y: point1.y }
+	drawFractal(point1)(pointA)(pointC)(color)(depth - 1)
+	drawFractal(pointA)(point2)(pointB)(color)(depth - 1)
+	drawFractal(pointC)(pointB)(point3)(color)(depth - 1)
 }
