f8db5167c776ec580dc071a71423e6eb597495eb
Ken Mueller <kenmueller0@gmail.com>
2019-04-27 21:11:13 -0700

    midpoint function

--- index.js
+++ index.js
@@ -16,11 +16,14 @@ const drawTriangle = point1 => point2 => point3 => color => {
 	drawLine(point3)(point1)(color)
 }
 
+const midpoint = point1 => point2 =>
+	({ x: (point1.x + point2.x) / 2, y: (point1.y + point2.y) / 2 })
+
 const drawFractal = point1 => point2 => point3 => color => depth => {
 	if (depth <= 0) return drawTriangle(point1)(point2)(point3)(color)
-	const pointA = { x: point1.x + (point2.x - point1.x) / 2, y: point1.y - (point1.y - point2.y) / 2 }
-	const pointB = { x: point2.x + (point3.x - point2.x) / 2, y: point2.y - (point2.y - point3.y) / 2 }
-	const pointC = { x: point1.x + (point3.x - point1.x) / 2, y: point1.y }
+	const pointA = midpoint(point1)(point2)
+	const pointB = midpoint(point2)(point3)
+	const pointC = midpoint(point3)(point1)
 	drawFractal(point1)(pointA)(pointC)(color)(depth - 1)
 	drawFractal(pointA)(point2)(pointB)(color)(depth - 1)
 	drawFractal(pointC)(pointB)(point3)(color)(depth - 1)
